ad8963bde007bddcb1409fb1b7e0f582f9f2944e
jotran <josephthientran@gmail.com>
2016-10-20 23:20:10 -0700

    Add test cases for handling errors with SteamApi

--- lib/steam_api.rb
+++ lib/steam_api.rb
@@ -19,7 +19,11 @@ class SteamApi
     url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/"\
           "?format=json&include_played_free_games=1&include_appinfo=1"\
           "&key=#{@steam_key}&steamid=#{@steam_id}"
-    games_hash = get_hash(url)['response']['games']
+    begin 
+      games_hash = get_hash(url)['response']['games']
+    rescue JSON::ParserError
+      games_hash = {}
+    end
   end
 
   private :get_hash
--- spec/lib/steam_api_spec.rb
+++ spec/lib/steam_api_spec.rb
@@ -3,9 +3,9 @@ require 'steam_api'
 
 RSpec.describe 'Steam API', :vcr do
   before :each do
-    key = Rails.application.secrets.STEAM_KEY
-    id = Rails.application.secrets.STEAM_ID
-    @steam_api = SteamApi.new(key, id)
+    @KEY = Rails.application.secrets.STEAM_KEY
+    @ID = Rails.application.secrets.STEAM_ID
+    @steam_api = SteamApi.new(@KEY, @ID)
   end
 
   it 'downloads and parses json' do 
@@ -14,8 +14,26 @@ RSpec.describe 'Steam API', :vcr do
     expect(hash).to be_an_instance_of(Hash)
   end
 
+  it 'throws an error if not json url' do
+    url = 'https://en.wikipedia.org/w/api.php?action=query&titles=json&format=xml'
+    expect { @steam_api.send(:get_hash, url) }.to raise_error(JSON::ParserError)
+  end
+
   it 'gets a user\'s games' do
     games = @steam_api.get_games
     expect(games).not_to be_empty
   end
+
+  it 'returns nothing with invalid user id' do
+    steam_api = SteamApi.new(@KEY, '123') 
+    games = steam_api.get_games
+    expect(games).to be_empty
+  end
+
+  it 'returns nothing with invalid api key' do
+    steam_api = SteamApi.new('123', @ID) 
+    games = steam_api.get_games
+    expect(games).to be_empty
+  end
+
 end
