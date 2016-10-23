55e3e83885bea61677280d2829004dac97ef0aa0
jotran <josephthientran@gmail.com>
2016-10-22 23:06:59 -0700

    Add show function to the Users controller
    The show function shows a random game for a given user.
    Add a blank show view and modify the routes so that the rspec test can
    run.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
+require 'steam_api'
+
 class UsersController < ApplicationController
+  include UsersHelper
+
   def index
   end
+
+  def show
+    api = SteamApi.new(Rails.application.secrets.STEAM_KEY, params[:id])
+    games = api.get_games
+    @game = get_random_game(games)
+  end
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
+  describe 'GET #index' do
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
+  describe 'GET #show', :vcr do
+    it 'assigns a random game for the user' do
+      get :show, params: {id: Rails.application.secrets.STEAM_ID}
+      expect(assigns(:game)).not_to be_nil
+    end
+  end
+  
 end
