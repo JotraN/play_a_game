49b71a8814d07c60e18af0eff7b61efb5700d4f3
jotran <josephthientran@gmail.com>
2016-10-27 23:35:26 -0700

    Remove the users portion of paths from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
  root 'users#index'
- get '/users/', to: 'users#index', :as => :index
- get '/users/search/', to: 'users#search', :as => :search
- get '/users/:id', to: 'users#show', :as => :show
+ get '/search/', to: 'users#search', :as => :search
+ get '/:id', to: 'users#show', :as => :show
 end
